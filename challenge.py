9b51869a3c1a66cc2a06746c0ea07cf9b1c3d3ec
diogomoreirax <rm99478@fiap.com.br>
2023-04-07 16:40:11 -0300

    mais alterações do dia

--- challenge.py
+++ challenge.py
@@ -10,39 +10,69 @@ while start == 1:
     veic = input("Qual a placa do veiculo: ")
 
 
-#PRIMEIRO VEMOS SE É URGENTE OU NÃO
+            #PRIMEIRO VEMOS SE O ATENTIMENTO É IMEDIATO OU DE AGENDAMENTO
     imediatouagendamento = int (input("Digite (1) para atendimento imediato ou (2) para agendamento:"))
     match imediatouagendamento:
         case 1:
             print("Atendimento imediato")
 
-            # VEMOS AQUI A SITUACAO
+            #CASO SEJA IMEDIATO, AQUI VEMOS O QUE ACONTECEU 
 
             situacao = int(input("Digite a informação que mais se adequa a situação sendo (1) Acidente de trânsito e (2) falha operacional"))
             match situacao:
+                #SITUAÇÃO 1 - ACIDENTE DE CARRO
                 case 1:
                     print("Acidente de trânsito")
-                    tipo_carro = int (input("O seu veiuculo é (1) leve/comum ou (2) pesado?"))
-                    if tipo_carro == 1: ##carro comum/leve
-                        end_leve = input("Qual endereço deveremos ir")
-                        tel_leve = input("Qual telefone devemos ligar?")
-                        print(f"O guinhco para o veiculo comum/leve de placa: {veic} do propietário de cpf: {cpf} está sendo enviado para o endereço {end_leve}")
 
+                    #AQUI PERGUNTAMOS SE O CARRO QUE SOFREU O ACIDENTE DE TRANSITO É LEVE OU COMUM
+
+                    tipo_carro = int (input("O seu veiuculo é (1) leve/comum ou (2) pesado?"))
+                    if tipo_carro == 1:
+
+                        
+                            #TRANSITO + LEVE
+                         
+                            end_leve = input("Qual endereço deveremos ir")
+                            tel_leve = input("Qual telefone devemos ligar?")
+                            print(f"O guinhco para o veiculo comum/leve de placa: {veic} do propietário de cpf: {cpf} está sendo enviado para o endereço {end_leve}")
+                            deseja_cont_leve_transito = input("Encerrar atendimento? (1) para SIM e (2) para NÃO")
+                            while deseja_cont_leve_transito == 1:
+                                print("fim do atentimento")   
+                            
+                           
+                            #TRANSITO + PESADO
                     elif tipo_carro == 2:
                         end_pesado = input("Qual endereço deveremos ir")
                         tel_pesado = input("Qual telefone devemos ligar?")
-
+                            #PERGUNTAMOS SE O DONO DO VEICULO PESADO JÁ TEM CONHECIMENTO DO TIPO DE GUINHCO QUE PRECISA
                         guincho_escolha = int (input("Você sabe que tipo de guincho precisaria? (1) - Sim e (2) Não"))
+                            #CASO SIM, NÓS PEDIMOS MAIS INFORMAÇÕES PARA CONFIRMAR E ENVIAMOS.
                         if guincho_escolha == 1:
-                            tipo_guincho = input("Descreva o tipo de guincho")
-
-
                             tipo_guincho = input("Digite o tipo de guinhco que nós encaminharemos para você")
+
+
+                            #CASO NÃO, NÓS PEDIMOS MAIS INFORMAÇÕES.
                         elif guincho_escolha == 2:
-                            print("Ok, iremos de auxiliar nos próximos passsos")
-                            
+                            print("Ok, iremos de auxiliar nos próximos passsos.")
+                            #TENTAMOS VER SE O DONO SABE ALGO SOBRE VEICULOS PESADOS PRIMEIRO MAS CASO NÃO, NÓS ENVIAMOS O AUXILIO TAMBÉM
+                            print("Digite de acordo com seu conhecimento as seguintes especificações se souber. (Caso não saiba, digite apenas 0 em cada uma")
 
+                            tipo_carroceria = int(input("Digite o tipo de carroceria"))
+                            chassi_alongado = int(input("Seu chassi é alongado?"))
+                            comprimento = int(input("Qual o comprimento do seu veiculo?"))
+                            peso_com_carga = int(input("Peso do veiculo com a carga"))
+                            peso_sem_carga = int(input("Peso do veiculo sem a carga"))
+                            quantidade_de_eixos = int(input("Qual a quantidade de eixo"))
 
+                            #SE O DONO NÃO SOUBER DE NADA, NÓS ENVIAMOS O AUXILIO.
+                            if tipo_carroceria and chassi_alongado and comprimento and peso_com_carga and peso_sem_carga and quantidade_de_eixos == 0:
+                                print(f"Iremos encaminha um de nossos atendentes para te auxiliar melhor. Confirme os dados abaixo: ")
+                                print(f"Guincho pedido pelo CPF/CNPJ: {cpf}")
+                                print(f"Ser atendido no endereço: {end_pesado}")
+                                print(f"Telefone para contato: {tel_pesado}")
+                                confirmacao_pessoa_nao_sabe = int (input("Digi"))
+                                print("Você confirma os dados abaixo?")
+                                #CONFIRMAÇÃO DE PEDIDO PARA O VEICULO PESADO, CASO O DONO NÃO SOUBER DE NADA
                             print(f"O guinhco para o veiculo pesado de placa: {veic} do propietário de cpf: {cpf} está sendo enviado para o endereço {end_pesado}")
                     else:
                         print("Tipo de carro inválido")
@@ -53,14 +83,65 @@ while start == 1:
 
                 case 2:
                     print("falha operacional")
+
+                    tipo_carro = int (input("O seu veiuculo é (1) leve/comum ou (2) pesado?"))
+                    if tipo_carro == 1: 
+                            #FALHA + LEVE
+                        end_leve = input("Qual endereço deveremos ir")
+                        tel_leve = input("Qual telefone devemos ligar?")
+                        print(f"O guinhco para o veiculo comum/leve de placa: {veic} do propietário de cpf: {cpf} está sendo enviado para o endereço {end_leve}")
+                            #FALHA + PESADO
+                    elif tipo_carro == 2:
+                        end_pesado = input("Qual endereço deveremos ir")
+                        tel_pesado = input("Qual telefone devemos ligar?")
+                            #PERGUNTAMOS SE O DONO DO VEICULO PESADO JÁ TEM CONHECIMENTO DO TIPO DE GUINHCO QUE PRECISA
+                        guincho_escolha = int (input("Você sabe que tipo de guincho precisaria? (1) - Sim e (2) Não"))
+
+                            #CASO SIM, PEDIMOS O MODELO E DETALHES DO VEICULO PARA CONFIRMAR
+                        if guincho_escolha == 1:
+                            tipo_guincho = input("Digite o tipo de guinhco que nós encaminharemos para você")
+
+
+                            #CASO NÃO, NÓS PEDIMOS MAIS INFORMAÇÕES.
+                        elif guincho_escolha == 2:
+                            print("Ok, iremos de auxiliar nos próximos passsos.")
+
+                            #TENTAMOS VER SE O DONO SABE ALGO SOBRE VEICULOS PESADOS PRIMEIRO MAS CASO NÃO, NÓS ENVIAMOS O AUXILIO TAMBÉM
+
+                            print("Digite de acordo com seu conhecimento as seguintes especificações se souber. (Caso não saiba, digite apenas 0 em cada uma")
+
+                            tipo_carroceria = int(input("Digite o tipo de carroceria"))
+                            chassi_alongado = int(input("Seu chassi é alongado?"))
+                            comprimento = int(input("Qual o comprimento do seu veiculo?"))
+                            peso_com_carga = int(input("Peso do veiculo com a carga"))
+                            peso_sem_carga = int(input("Peso do veiculo sem a carga"))
+                            quantidade_de_eixos = int(input("Qual a quantidade de eixo"))
+
+                            #SE O DONO NÃO SOUBER DE NADA, NÓS ENVIAMOS O AUXILIO.
+                            if tipo_carroceria and chassi_alongado and comprimento and peso_com_carga and peso_sem_carga and quantidade_de_eixos == 0:
+                                print(f"Iremos encaminha um de nossos atendentes para te auxiliar melhor. Confirme os dados abaixo: ")
+                                print(f"Guincho pedido pelo CPF/CNPJ: {cpf}")
+                                print(f"Ser atendido no endereço: {end_pesado}")
+                                print(f"Telefone para contato: {tel_pesado}")
+                                confirmacao_pessoa_nao_sabe = int (input("Digi"))
+                                print("Você confirma os dados abaixo?")
+                                #CONFIRMAÇÃO DE PEDIDO PARA O VEICULO PESADO, CASO O DONO NÃO SOUBER DE NADA
+                                print(f"O guinhco para o veiculo pesado de placa: {veic} do propietário de cpf: {cpf} está sendo enviado para o endereço {end_pesado}")
+                                print("Atendimento encerrado")
+                            else:
+                                print("Analisaremos as informações e enviaremos o guincho para o")
+                    else:
+                        print("tipo de carro inválido")
+
+        #CASO AGENDAMENTO        
         case 2:
-            print("Agendamento: OBS: só realizamos agendaentos  dentro do mês de pedido")
+            print("Agendamento: OBS: só realizamos agendamentos dentro do mês do pedido")
             data_agendamento = input("Para qual data gostaria de agendar")
             hora_agendamento = input("Que horas seria feito o atendimento?")
             end_agendamento = input("Qual endereço deveremos ir")
             tel_agendamento = input("Qual telefone devemos ligar?")
             print(f"Seu guinhcho foi agendado para dia {data_agendamento} ás {hora_agendamento} para o endereço {end_agendamento}")
             print("Fim do atendimento")
-    break
+    
 
-print("Você desejou não começar")
+print("Atendimento encerrado")
